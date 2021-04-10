c6b156ca94c60bc8435cdb0089b91cea3ac94279
geniushyeon <jhlee0912@icloud.com>
2021-04-10 12:18:21 +0900

    [ADD] Jamie - first commit

---
+++ Jamie/chap01/react0410/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+import React from 'react';
+import Hello from './Hello';
+
+function App() {
+  return (
+    <div>
+      <Hello />
+
+    </div>
+  );
+}
+
+export default App;
---
+++ Jamie/chap01/react0410/src/Hello.js
@@ -0,0 +1,11 @@
+// 리액트 컴포넌트 만들기
+
+// 리액트 불러오기
+import React from 'react';
+
+function Hello() {
+    return <div>Hello React!</div>
+}
+
+// Hello라는 컴포넌트를 내보낼거야
+export default Hello;
