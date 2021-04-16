c400f3b408eedf3b4bd979ef6070c7ee7e5877b4
geniushyeon <jhlee0912@icloud.com>
2021-04-16 17:30:29 +0900

    [UPDATE] 1~15장 복습

--- Jamie/chap01/react0410/src/App.js
+++ Jamie/chap01/react0410/src/App.js
@@ -1,87 +1,83 @@
-import './App.css';
-
-import React, { useRef, useState } from 'react';
+import React, { useState, useRef } from 'react';
 import UserList from './UserList';
-import CreateUser from './CreateUser'
+import CreateUser from './CreateUser';
 
 function App() {
-  const [inputs, setInputs] = useState({
-    username: '',
-    email: ''
-  });
+	const [inputs, setInputs] = useState({
+		username : '',
+		email : ''
+	});
 
-  const { username, email } = inputs;
-  const onChange = e => {
-    const { name, value } = e.target;
-    setInputs({
-      ...inputs,
-      [name] : value
-    });
-  };
+	const {username, email} = inputs;
+	const onChange = e => {
+		const {name, value} = e.target;
+		setInputs({
+			...inputs,
+			[name] : value
+		});
+	};
 
-  const [users, setUsers] = useState([
-    {
-      id: 1,
-      username: 'jamie',
-      email: 'jamie@gmail.com',
-      active: true
-    },
-    {
-      id: 2,
-      username: 'diana',
-      email: 'diana@gmail.com',
-      active: false
-    },
-    {
-      id: 3,
-      username: 'yannus',
-      email: 'yannus@gmail.com',
-      active: false
-    }
-  ]);
+	const [users, setUsers] = useState([
+		{
+			id: 1,
+			username: 'jamie',
+			email: 'jamie@gmail.com',
+			active : true
+		},
+		{
+			id: 2,
+			username: 'chicol',
+			email: 'chicol@gmail.com',
+			active : false
+		},
+		{
+			id: 3,
+			username: 'coco',
+			email: 'coco@gmail.com',
+			active : true
+		}
+	]);
+	const nextId = useRef(4);
 
-  const nextId = useRef(4);
-  const onCreate = () => {
-    // 나중에 구현할 배열에 항목을 추가하는 로직
-    const user = {
-      id : nextId.current,
-      username,
-      email
-    };
+	const onCreate = () => {
+		const user = {
+			id : nextId.current,
+			username,
+			email
+		};
+		setUsers([...users, user]);
 
-    setUsers([...users, user]);
-    // setUsers(users.concat(user));
-    setInputs({
-      username: '',
-      email: ''
-    });
-    nextId.current += 1;
-  };
+		setInputs({
+			username: '',
+			email : ''
+		});
+		nextId.current += 1;
+	};
 
-  const onRemove = id => {
-    // user.id가 파라미터로 일치하지 않는 원소들만 추출하여 새로운 배열을 만듦
-    // = user.id가 id인 것을 제거
-    setUsers(users.filter(user => user.id !== id));
-  };
+	const onRemove = id => {
+		// user.id가 파라미터로 일치하지 않는 원소들만 추출하여 새로운 배열을 만듦.
+		// == user.id가 id와 일치하는 것을 빼고 원소 추출 -> 새로운 배열 만들기
+		setUsers(users.filter(user => user.id !== id));
+	};
 
-  const onToggle = id => {
-    setUsers(
-      users.map(user =>
-        user.id === id ? {...user, active : !user.active} : user
-      )
-    )
-  };
-  return (
-    <>
-      <CreateUser 
-        username={username} 
-        email={email} 
-        onChange={onChange}
-        onCreate={onCreate}
-      />
-      <UserList users={users} onRemove={onRemove} onToggle={onToggle}/>
-    </>
-  );
+	const onToggle = id => {
+		setUsers(
+			users.map(user => 
+				user.id === id ? {...user, active: !user.active} : user
+				)
+		)
+	}
+	return (
+		<>
+			<CreateUser 
+				username={username} 
+				email={email} 
+				onChange={onChange}
+				onCreate={onCreate}
+			/>
+			<UserList users={users} onRemove={onRemove} onToggle={onToggle}/>
+		</>
+	);
 
 }
 
