590e38a6249a3b92b089192c73e274c3520912cc
taltla1028 <75877260+taltla1028@users.noreply.github.com>
2021-04-17 15:51:17 +0900

    0417 복습

--- Yanuus/react0415/src/App.js
+++ Yanuus/react0415/src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+
+import React from 'react';
+import Yanuus from './Yanuus';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <div>
+     <Yanuus />
+   </div>
   );
 }
 
 export default App;
+// 컴포넌트는 일종의 UI 조각입니다. 그리고 쉽게 재사용도 가능하다
--- Yanuus/react0415/src/index.js
+++ Yanuus/react0415/src/index.js
@@ -2,16 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
+  // 여기서 ReactDOM.render의 역할은 브라우저에 있는 실제 DOM 내부에 리액트 컴포넌트 렌더링 하겠다는것을 의미
   <React.StrictMode>
     <App />
+    
   </React.StrictMode>,
   document.getElementById('root')
+  // id가 root인 DOM을 선택하고 있는데 이 DOM 은 public/index.html 에 있다.
 );
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
