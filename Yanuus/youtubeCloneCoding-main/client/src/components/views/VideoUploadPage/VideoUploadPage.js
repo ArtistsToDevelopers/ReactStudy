f6d41f7ffd747138209f86cde94183893390837a
taltla1028 <75877260+taltla1028@users.noreply.github.com>
2021-05-01 11:19:21 +0900

    유튜브 클론코딩

--- Yanuus/youtubeCloneCoding-main/client/src/components/views/VideoUploadPage/VideoUploadPage.js
+++ Yanuus/youtubeCloneCoding-main/client/src/components/views/VideoUploadPage/VideoUploadPage.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { Typography, Button, Form, message, Input, Icon } from 'antd';
 import Dropzone from 'react-dropzone';
 import Axios from 'axios';
+import { useSelector} from 'react-redux';
 
 const { TextArea } = Input ;
 const { Title } = Typography;
@@ -16,13 +17,15 @@ const CategoryOptions = [
     { value: 2, label: "Music" },
     { value: 3, label: "Pets & Animals " },
 ]
-function VideoUploadPage(){
-    
+function VideoUploadPage(props){
+    const user = useSelector(state => state.user);
     const [VideoTitle, setVideoTitle] = useState("")
     const [Description, setDescription] = useState("")
     const [Private, setPrivate] = useState(0)
     const [Category, setCategory] = useState("Film & Animation")
-
+    const [FilePath,setFilePath] = useState("")
+    const [Duration, setDuration] = useState("")
+    const [ThumbnailPath,setThumbnailPath] = useState("")
     const onTitleChange = (e) => {
         setVideoTitle(e.currentTarget.value)
     }
@@ -46,29 +49,82 @@ function VideoUploadPage(){
         }
         formData.append("file",files[0])
         Axios.post('/api/video/uploadfiles', formData, config)
-        .then(response =>{ 
+        .then(response => { 
             if(response.data.success){
                 console.log(response.data)
+
+                let variable = {
+                    
+                    url:response.data.url,
+                    fileName: response.data.fileName
+                }
+
+                setFilePath(response.data.url)
+
+                Axios.post('/api/video/thumbnail',variable)
+                .then(response => {
+                    if(response.data.success){
+
+                        setDuration(response.data.fileDuration)
+                        setThumbnailPath(response.data.url)
+                        
+                    }else{
+                        alert('썸내일 생성에 실패했습니다.')
+                    }
+                })
             }else{
                 alert('비디오 업로드를 실패했습니다.')
             }
         })
-       
     }
+
+    const onSumit = (e) => {
+        e.preventDefault();
+        
+        const variable = {
+            writer: user.userData._id,
+            title: VideoTitle,
+            description: Description,
+            private: Private,
+            filePath: FilePath,
+            category: Category,
+            duration: Duration,
+            thumbnail: ThumbnailPath,
+        }
+        
+        Axios.post('/api/video/uploadVideo',variable)
+            .then(response=> {
+                if(response.data.success){
+                   
+                    message.success('성공적으로 업로드를 했습니다.')
+
+                    setTimeout(() => {
+                        props.history.push('/')
+                        
+                    }, 3000);
+
+                }else{
+                    alert('비디오 업로드에 실패 했습니다.')
+                }
+            })
+    }
+
     return(
         <div style={{ maxWidth:'700px',margin:'2rem auto'}}>
             < div style={{ textAlign:'center',marginBottom:'2rem'}}>
                 <Title level={2}>Upload Video</Title>
             </div>
 
-            <Form onSubmit>
+            <Form onSubmit={onSumit}>
                 <div style={{ display:'flex', justifyContent: 'space-between' }}>
                         {/* Drop zone */}
 
                         <Dropzone 
                         onDrop={onDrop}
                         multiple={false}
-                        maxSize={1000000000}>
+                        maxSize={1000000000}
+                        >
+
                         {({ getRootProps, getInputProps}) => (
                             <div style={{ width: '300px', height: '240px', border:'1px solid lightgray', display:'flex',
                         alignItems:'center', justifyContent:'center'}} {...getRootProps()}>
@@ -79,9 +135,11 @@ function VideoUploadPage(){
 
                         </Dropzone>
                         {/* Thumbnail */}
-                        <div>
-                            <img src alt/>
-                        </div>
+                        {ThumbnailPath &&
+                            <div>
+                                <img src={`http://localhost:5000/${ThumbnailPath}`} alt="thumbnail"/>
+                            </div>
+                        }
                 </div>
                 <br />
                 <br />
@@ -115,7 +173,7 @@ function VideoUploadPage(){
                 </select>
                 <br />
                 <br />
-                <Button type="primary" size="large" onClick>
+                <Button type="primary" size="large" onClick={onSumit}>
                     Submit
                 </Button>
             </Form>
--- Yanuus/youtubeCloneCoding-main/server/routes/video.js
+++ Yanuus/youtubeCloneCoding-main/server/routes/video.js
@@ -1,17 +1,17 @@
 const express = require('express');
 const router = express.Router();
-// const { Video } = require("../models/User");
+const { Video } = require("../models/Video");
 
 const { auth } = require("../middleware/auth");
 const multer = require("multer");
-
+var ffmpeg = require("fluent-ffmpeg");
 // STRAGE MULTER CONFIG
 let storage = multer.diskStorage({
     destination: (req, file, cb) => {
         cb(null, "uploads/");
     },
     filename: (req, file, cb) => {
-        cd(null, `${Date.now()}_${file.originalname}`);
+        cb(null, `${Date.now()}_${file.originalname}`);
     },
     fileFilter: (req, file, cb) => {
         const ext = path.extname(file.originalname)
@@ -39,4 +39,75 @@ router.post('/uploadfiles',(req, res) => {
     })
 
 })
+
+
+router.post('/uploadVideo',(req, res) => {
+
+    //비디오 정보들을 저장한다.,
+    const video = new Video(req.body)
+
+    video.save((err, doc) => {
+        if(err) return res.json({ success: false, err })
+        res.status(200).json({ success: true })
+    })
+
+})
+
+router.get('/getVideos', (req, res) => {
+
+  //비디오를 DB 에서 가져와서 클라이언트에 보낸다.
+
+    Video.find()
+        .populate('writer')
+        .exec((err, videos)=> {
+            if(err) return res.status(400).send(err);
+            res.status(200).json({ success: true, videos })
+        })
+
+
+
+})
+
+
+router.post('/thumbnail', (req, res) => { 
+
+   // 썸내일 생성 하고 비디오 러닝타임도 가져오기
+   
+   let filePath = ""
+   let fileDuration = ""
+
+   // 비디오 정보 가져오기
+    ffmpeg.ffprobe(req.body.url, function(err, metadata){
+        console.dir(metadata); // all metadata
+        console.log(metadata.format.duration);
+        fileDuration = metadata.format.duration
+    });
+
+   // 썸내일 생성
+    ffmpeg(req.body.url)
+    .on('filenames', function (filenames) {
+        console.log('Will generate ' + filenames.join(','))
+        console.log(filenames)
+
+        filePath = "uploads/thumbnails/" + filenames[0]
+    })
+    .on('end',function() {
+        console.log('Screenshots taken');
+        return res.json({ success: true, url: filePath, fileDuration: fileDuration});
+    })
+    .on('error', function (err){
+        console.error(err);
+        return res.json({success: false, err });
+    })
+
+    .screenshot({
+        // will take screenshots at 20%, 40%, 60% and 80% of the video
+        count: 3,
+        folder: 'uploads/thumbnails',
+        size: '320x240',
+        filename: 'thumbnail-%b.png'
+    })
+
+})
+
 module.exports = router;
