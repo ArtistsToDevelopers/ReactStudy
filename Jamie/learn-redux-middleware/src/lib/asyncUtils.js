e4cf9349b0d8e7fbca9cad929627e27aee2e0d3e
geniushyeon <jhlee0912@icloud.com>
2021-05-17 22:11:40 +0900

    ✨ react-thunk로 promise 다루기

--- Jamie/learn-redux-middleware/src/lib/asyncUtils.js
+++ Jamie/learn-redux-middleware/src/lib/asyncUtils.js
@@ -11,9 +11,66 @@ export const createPromiseThunk = (type, promiseCreator) => {
     try {
       // 결과물 이름을 payload로 통일
       const payload = await promiseCreator(param);
-      dispatch({ type: SUCCESS, payload }) 
+      dispatch({ type: SUCCESS, payload })
     } catch (e) {
-      dispatch({ type: ERROR, payload: e, error: true})
+      dispatch({ type: ERROR, payload: e, error: true })
+    }
+  }
+}
+
+// reducer에서 사용할 수 있는 여러 유틸 함수들
+export const reducerUtils = {
+  // 초기 상태. 초기 data값은 기본적으로 Null이지만 바꿀 수도 있다.
+  initial: (initialData = null) => ({
+    loading: false,
+    data: initialData,
+    error: null
+  }),
+  // 로딩 중 상태. prevState의 경우에는 기본값은 null이지만
+  // 따로 값을 지정하면 Null로 바꾸지 않고 다른 값을 유지시킬 수 있다.
+  loading: (prevState = null) => ({
+    loading: true,
+    data: prevState,
+    error: null
+  }),
+  // 성공 상태
+  success: payload => ({
+    loading: false,
+    data: payload,
+    error: null
+  }),
+  // 실패 상태
+  error: error => ({
+    loading: false,
+    data: null,
+    error: error
+  })
+}
+
+// 비동기 관련 액션들을 처리하는 리듀서 생성
+// type은 action.type, key는 상태의 key
+export const handleAsyncActions = (type, key) => {
+  const [SUCCESS, ERROR] = [`${type}_SUCCESS`, `${type}_ERROR`]
+
+  return (state, action) => {
+    switch (action.type) {
+      case type:
+        return {
+          ...state,
+          [key]: reducerUtils.loading()
+        }
+      case SUCCESS:
+        return {
+          ...state,
+          [key]: reducerUtils.success(action.payload)
+        }
+      case ERROR:
+        return {
+          ...state,
+          [key]: reducerUtils.error(action.payload)
+        }
+      default:
+        return state
     }
   }
 }
--- Jamie/learn-redux-middleware/src/modules/posts.js
+++ Jamie/learn-redux-middleware/src/modules/posts.js
@@ -1,110 +1,37 @@
 import * as postsAPI from '../api/posts' // /api/posts 안의 함수 모두 불러오기
+import { createPromiseThunk, handleAsyncActions, reducerUtils } from '../lib/asyncUtils'
 
 /* action type */
-
 // post 여러 개 조회하기
-const GET_POSTS = 'GET_POSTS'
-const GET_POSTS_SUCCESS = 'GET_POSTS_SUCCESS'
-const GET_POSTS_ERROR = 'GET_POSTS_ERROR'
+const GET_POSTS = 'GET_POSTS';
+const GET_POSTS_SUCCESS = 'GET_POSTS_SUCCESS';
+const GET_POSTS_ERROR = 'GET_POSTS_ERROR';
 
 // post 하나 조회하기
-const GET_POST = 'GET_POST'
-const GET_POST_SUCCESS = 'GET_POST_SUCCESS'
-const GET_POST_ERROR = 'GET_POST_ERROR'
+const GET_POST = 'GET_POST';
+const GET_POST_SUCCESS = 'GET_POST_SUCCESS';
+const GET_POST_ERROR = 'GET_POST_ERROR';
 
-export const getPosts = () => async dispatch => {
-  dispatch({ type: GET_POSTS })
+export const getPosts = createPromiseThunk(GET_POSTS, postsAPI.getPosts)
+export const getPost = createPromiseThunk(GET_POST, postsAPI.getPostById)
 
-  try {
-    const posts = await postsAPI.getPosts()
-    dispatch({ type: GET_POSTS_SUCCESS, posts })
-  } catch (e) {
-    dispatch({ type: GET_POSTS_ERROR, error: e })
-  }
-}
-
-// thunk 함수에서도 파라미터 받아와서 사용 가능
-export const getPost = id => async dispatch => {
-  dispatch({ type: GET_POST })
-
-  try {
-    const post = await postsAPI.getPostById(id)
-    dispatch({ type: GET_POST_SUCCESS, post })
-  } catch (e) {
-    dispatch({ type: GET_POST_ERROR, error: e })
-  }
-}
-
+// initial() 함수를 사용하여 리팩토링
 const initialState = {
-  posts: {
-    loading: false,
-    data: null,
-    error: null
-  },
-  port: {
-    loading: false,
-    data: null,
-    error: null
-  }
+  posts: reducerUtils.initial(),
+  post: reducerUtils.initial()
 }
 
 export default function posts(state = initialState, action) {
   switch (action.type) {
     case GET_POSTS:
-      return {
-        ...state,
-        posts: {
-          loading: true,
-          data: null,
-          error: null
-        }
-      }
     case GET_POSTS_SUCCESS:
-      return {
-        ...state,
-        posts: {
-          loading: true,
-          data: action.posts,
-          error: null
-        }
-      }
     case GET_POSTS_ERROR:
-      return {
-        ...state,
-        posts: {
-          loading: true,
-          data: null,
-          error: action.error
-        }
-      }
+      return handleAsyncActions(GET_POSTS, 'posts')(state, action);
     case GET_POST:
-      return {
-        ...state,
-        post: {
-          loading: true,
-          data: null,
-          error: null
-        }
-      }
     case GET_POST_SUCCESS:
-      return {
-        ...state,
-        post: {
-          loading: true,
-          data: action.post,
-          error: null
-        }
-      }
     case GET_POST_ERROR:
-      return {
-        ...state,
-        post: {
-          loading: true,
-          data: null,
-          error: action.error
-        }
-      }
+      return handleAsyncActions(GET_POST, 'post')(state, action);
     default:
-      return state
+      return state;
   }
 }
