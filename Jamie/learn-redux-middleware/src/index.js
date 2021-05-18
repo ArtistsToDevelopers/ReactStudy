c61123536e57d01ba46f61f7ae88c3a9cf0cae32
geniushyeon <jhlee0912@icloud.com>
2021-05-19 00:42:47 +0900

    ✨ redux-saga

--- Jamie/learn-redux-middleware/src/index.js
+++ Jamie/learn-redux-middleware/src/index.js
@@ -5,20 +5,29 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { createStore, applyMiddleware } from 'redux'
 import { Provider } from 'react-redux'
-import rootReducer from './modules'
+import rootReducer, { rootSaga } from './modules'
 import logger from 'redux-logger'
 import { composeWithDevTools } from 'redux-devtools-extension'
 import ReduxThunk from 'redux-thunk'
 import { BrowserRouter, Router } from 'react-router-dom'
 import { createBrowserHistory } from 'history'
+import createSagaMiddleware from 'redux-saga'
 
 const customHistory = createBrowserHistory()
+const sagaMiddleware = createSagaMiddleware(); // saga middleware 생성
 
 const store = createStore(
   rootReducer,
   // logger를 사용하는 경우 logger가 맨 마지막에 와야 함
-  composeWithDevTools(applyMiddleware(ReduxThunk.withExtraArgument({ history: customHistory }), logger))
+  composeWithDevTools(
+    applyMiddleware(
+      ReduxThunk.withExtraArgument({ history: customHistory }),
+      sagaMiddleware,
+      logger))
 )
+// 스토어 생성 후
+sagaMiddleware.run(rootSaga) // root saga 실행
+
 
 ReactDOM.render(
   <Router history={customHistory}>
--- Jamie/learn-redux-middleware/src/modules/index.js
+++ Jamie/learn-redux-middleware/src/modules/index.js
@@ -1,10 +1,15 @@
 import { combineReducers } from 'redux'
-import counter from './counter'
+import counter, { counterSaga } from './counter'
 import posts from './posts'
+import { all } from 'redux-saga/effects'
 
-const rootReducer = combineReducers({ 
+const rootReducer = combineReducers({
   counter,
   posts
 })
+
+export function* rootSaga() {
+  yield all([counterSaga()]); // 배열 안의 여러 saga를 동시에 실행
+}
 
 export default rootReducer
